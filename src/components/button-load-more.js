fe86c30c0bdb01429b3afb525c2f5f343bde89fd
grigorievk <grigorievka@gmail.com>
2019-08-25 16:54:01 +0300

    Split main.js to component modules. Fix render function for exported function string return. Install npm dependencies, config them.

---
+++ src/components/button-load-more.js
@@ -0,0 +1,3 @@
+export const createButtonLoadMoreTemplate = () => {
+  return `<button class="load-more" type="button">load more</button>`;
+};
---
+++ src/components/content.js
@@ -0,0 +1,5 @@
+export const createContentTemplate = () => {
+  return `<section class="board container">
+         <div class="board__tasks"></div>
+    </section>`;
+};
